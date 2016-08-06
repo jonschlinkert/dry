f14bf24d7d3ccd60f34b553ff6858940003ba218
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-06 11:52:55 -0400

    expose `render`

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 'use strict';
 
-module.exports = function() {
-};
+var debug = require('debug')('dry');
+var compile = require('./lib/compile');
+var parser = require('./lib/parser');
+var utils = require('./lib/utils');
+
+/**
+ * Render
+ */
+
+function render(file, locals) {
+  debug('rendering <%s>', file.path);
+  compile(file, locals);
+  file.fn(locals);
+  return file;
+}
+
+module.exports = render;
+module.exports.parse = parser;
+module.exports.render = render;
+module.exports.compile = compile;
